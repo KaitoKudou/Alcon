0e98b7a81d81fd5f44bf4bb49adaaf2d5ab8809b
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-27 03:19:20 +0900

    [Add]必須メソッドを追加

--- alcon/alcon/Views/GoalView.swift
+++ alcon/alcon/Views/GoalView.swift
@@ -5,4 +5,14 @@
 //  Created by 工藤海斗 on 2021/03/26.
 //
 
-import Foundation
+import UIKit
+
+class GoalView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
