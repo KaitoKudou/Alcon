00c616c9ac804c6dee37bf175cab2644c8aced1c
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-27 23:15:23 +0900

    [Add]navigationItemのtitleを設定

--- alcon/alcon/ViewControllers/SettingsViewController.swift
+++ alcon/alcon/ViewControllers/SettingsViewController.swift
@@ -12,7 +12,7 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
-        title = "設定"
+        navigationItem.title = "設定"
         print("SettingsViewController")
     }
 }
