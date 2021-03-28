2f4d60d28813ce19f80831cabffd78c81a2cfffe
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-28 16:12:13 +0900

    [Add]必須メソッドを追加

--- alcon/alcon/Views/CalendarView.swift
+++ alcon/alcon/Views/CalendarView.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import FSCalendar
+
+class CalendarView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
