8206fc4c9c4cc3fc64ebadbd20b0d138edeb0637
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-30 02:49:14 +0900

    [Add]TableView作成

--- alcon/alcon/Views/DrinkTableView.swift
+++ alcon/alcon/Views/DrinkTableView.swift
@@ -5,4 +5,27 @@
 //  Created by 工藤海斗 on 2021/03/26.
 //
 
-import Foundation
+import UIKit
+
+class DrinkTableView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTableView() {
+        var drinkTableView: UITableView = UITableView()
+        drinkTableView.backgroundColor = UIColor(hex: "E0E0E0")
+        addSubview(drinkTableView)
+        drinkTableView.translatesAutoresizingMaskIntoConstraints = false
+        drinkTableView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        drinkTableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        drinkTableView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        drinkTableView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        
+    }
+}
