95fe2e7936b43e9d5273fb5747a46dd11a09aa06
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-29 21:32:53 +0900

    [Add]必須メソッドを追加

--- alcon/alcon/Views/CalendarView.swift
+++ alcon/alcon/Views/CalendarView.swift
@@ -8,12 +8,30 @@
 import Foundation
 import FSCalendar
 
-class CalendarView: UIView {
+class CalendarView: UIView, FSCalendarDataSource, FSCalendarDelegate {
+    fileprivate weak var calendar: FSCalendar!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupCalendar()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCalendar() {
+        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: frame.width, height: 300))
+        calendar.dataSource = self
+        calendar.delegate = self
+        calendar.backgroundColor = UIColor(hex: "C3E2FF")
+        addSubview(calendar)
+        self.calendar = calendar
+        
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        calendar.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        calendar.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        calendar.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        calendar.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
     }
 }
