e392f56f4bd6c43aaf5a673d588734431817a820
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-02 03:01:18 +0900

    [Add]TableViewに仮データ反映

--- alcon/alcon/Views/DrinkTableView.swift
+++ alcon/alcon/Views/DrinkTableView.swift
@@ -7,25 +7,48 @@
 
 import UIKit
 
-class DrinkTableView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setTableView()
+class DrinkTableView: UITableView {
+    
+    var drinkNameArray: [String] = ["ワイン(グラス)", "ビール350ml", "ハイボール"]
+    var capacityArray: [Int] = [120, 350, 350]
+    var pureAlcoholArray: [Int] = [11, 14, 25]
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: .plain)
+        setLayoutTableView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setTableView() {
-        var drinkTableView: UITableView = UITableView()
-        drinkTableView.backgroundColor = UIColor(hex: "E0E0E0")
-        addSubview(drinkTableView)
-        drinkTableView.translatesAutoresizingMaskIntoConstraints = false
-        drinkTableView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        drinkTableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        drinkTableView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        drinkTableView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        
+    private func setLayoutTableView() {
+        backgroundColor = UIColor(hex: "E0E0E0")
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        register(DrinkTableViewCell.self, forCellReuseIdentifier: "cell")
+        delegate = self
+        dataSource = self
+    }
+}
+
+extension DrinkTableView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! DrinkTableViewCell
+        cell.backgroundColor = UIColor(hex: "E0E0E0")
+        cell.setCell(drinkName: drinkNameArray[indexPath.row], capacity: capacityArray[indexPath.row
+        ], pureAlcohol: pureAlcoholArray[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
 }
