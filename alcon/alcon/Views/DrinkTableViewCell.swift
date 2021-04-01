44aa985b946bf0a650d1610f310048b21a0a909d
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-02 03:00:16 +0900

    [Add]カスタムセル作成

---
+++ alcon/alcon/Views/DrinkTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DrinkTableViewCell.swift
+//  alcon
+//
+//  Created by 工藤海斗 on 2021/04/02.
+//
+
+import UIKit
+
+class DrinkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
