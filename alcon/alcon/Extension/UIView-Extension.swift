a3bf76608c9ffe9edadca8a4e7bfe0c8b88f3565
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-03 19:27:36 +0900

    [Add]Anchorを設定するextensionを作成

--- alcon/alcon/Extension/UIView-Extension.swift
+++ alcon/alcon/Extension/UIView-Extension.swift
@@ -5,4 +5,56 @@
 //  Created by 工藤海斗 on 2021/04/03.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func anchor(
+        top: NSLayoutYAxisAnchor? = nil,
+        bottom: NSLayoutYAxisAnchor? = nil,
+        left: NSLayoutXAxisAnchor? = nil,
+        right: NSLayoutXAxisAnchor? = nil,
+        centerY: NSLayoutYAxisAnchor? = nil,
+        centerX: NSLayoutXAxisAnchor? = nil,
+        width: CGFloat? = nil,
+        height: CGFloat? = nil,
+        topPadding: CGFloat = 0,
+        bottomPadding: CGFloat = 0,
+        leftPadding: CGFloat = 0,
+        rightPadding: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: topPadding).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -bottomPadding).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: leftPadding).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -rightPadding).isActive = true
+        }
+        
+        if let centerY = centerY {
+            centerYAnchor.constraint(equalTo: centerY).isActive = true
+        }
+        
+        if let centerX = centerX {
+            centerXAnchor.constraint(equalTo: centerX).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
