4baba835fb9b10cd4d608d04e27b93f9a43d61b3
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-05 02:56:32 +0900

    [Add]TextViewに文字数制限をつけた

--- alcon/alcon/ViewControllers/GoalChangeViewController.swift
+++ alcon/alcon/ViewControllers/GoalChangeViewController.swift
@@ -5,6 +5,8 @@
 //  Created by 工藤海斗 on 2021/04/04.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class GoalChangeViewController: UIViewController {
@@ -12,12 +14,14 @@ class GoalChangeViewController: UIViewController {
     var characterCountLabel: UILabel!
     var goalTextView: UITextView!
     var goalStorageButton: UIButton!
+    private let disposeBag = DisposeBag()
+    private let maxLength: Int = 30
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationItem.title = "目標変更"
-        characterCountLabel = DrinkInfoLabel(text: "30/30", fontSize: 20, alignment: .left)
+        characterCountLabel = DrinkInfoLabel(text: "0/\(maxLength)", fontSize: 20, alignment: .left)
         goalTextView = CustomTextView(backgroundColor: UIColor(hex: "F2F2F7"), font: UIFont.systemFont(ofSize: 20), borderColor: UIColor(hex: "D0D0D1").cgColor, borderWidth: 1.0, cornerRadius: 10.0)
         goalStorageButton = CustomButton(title: "保存", titleColor: UIColor(hex: "73AADD"), titileFont: UIFont.boldSystemFont(ofSize: 20), backgroundColor: UIColor(hex: "C3E2FF"), cornerRadius: 10)
         
@@ -29,6 +33,8 @@ class GoalChangeViewController: UIViewController {
         goalTextView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
         goalTextView.anchor(top: characterCountLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,topPadding: 20, leftPadding: 20, rightPadding: 20)
         goalStorageButton.anchor(bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, height: view.bounds.height * 0.1 - 30, bottomPadding: 20, leftPadding: 20, rightPadding: 20)
+        
+        setBindings()
     }
     
     //入力画面またはkeyboardの外を押したら、キーボードを閉じる処理
@@ -36,5 +42,43 @@ class GoalChangeViewController: UIViewController {
         if (goalTextView.isFirstResponder) {
             goalTextView.resignFirstResponder()
         }
+    }
+    
+    private func setBindings() {
+        goalTextView.rx.text
+            .asDriver()
+            .drive { [weak self] (text) in
+                guard let self = self else { return }
+                if let text = text, text.count >= self.maxLength {
+                    self.characterCountLabel.text = "\(text.count)/30"
+                    self.goalTextView.textColor = .red
+                    self.characterCountLabel.textColor = .red
+                } else {
+                    self.goalTextView.textColor = .black
+                    self.characterCountLabel.textColor = .black
+                }
+                
+                let inputTextLength = self.goalTextView.text.count
+                self.characterCountLabel.text = "\(inputTextLength)/30"
+                
+                if inputTextLength <= 30 {
+                    self.goalStorageButton.isEnabled = true
+                    self.goalStorageButton.backgroundColor = UIColor(hex: "C3E2FF")
+                    self.goalStorageButton.setTitleColor(UIColor(hex: "73AADD"), for: .normal)
+                } else {
+                    self.goalStorageButton.isEnabled = false
+                    self.goalStorageButton.backgroundColor = UIColor(hex: "F2F2F7")
+                    self.goalStorageButton.setTitleColor(UIColor(hex: "B2B2B2"), for: .normal)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        goalStorageButton.rx.tap
+            .asDriver()
+            .drive { [weak self] _ in
+                guard let self = self else { return }
+                self.navigationController?.popViewController(animated: true)
+            }
+            .disposed(by: disposeBag)
     }
 }
