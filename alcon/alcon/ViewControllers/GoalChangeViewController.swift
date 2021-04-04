a42bfce6eb258fb59029e25678657c11353f99d2
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-04 20:03:51 +0900

    [Add]ファイル追加

---
+++ alcon/alcon/ViewControllers/GoalChangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GoalChangeViewController.swift
+//  alcon
+//
+//  Created by 工藤海斗 on 2021/04/04.
+//
+
+import UIKit
+
+class GoalChangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ alcon/alcon/Views/CustomTextView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTextView.swift
+//  alcon
+//
+//  Created by 工藤海斗 on 2021/04/04.
+//
+
+import Foundation
