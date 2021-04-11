127793739c8260d2f926e8fe55f86d574b7dc643
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-12 03:25:57 +0900

    [Add]Firestoreからお酒一覧取得

--- alcon/alcon/ViewModels/DrinkRecordViewModel.swift
+++ alcon/alcon/ViewModels/DrinkRecordViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by 工藤海斗 on 2021/04/12.
 //
 
+import FirebaseFirestore
 import Foundation
+
+class DrinkRecordViewModel {
+    
+    func fetchDrinkLists(completion: @escaping ([Drinks]) -> Void) {
+        let firestore = Firestore.firestore()
+        firestore.collection("drinkLists").getDocuments { [weak self] (querySnapshots, err) in
+            guard self != nil else { return }
+            
+            if let err = err {
+                print("お酒一覧情報読み込みエラー: \(err)")
+                return
+            }
+            
+            let drinks = querySnapshots?.documents.map({ (snapshot) -> Drinks in
+                let dic = snapshot.data()
+                let drink: Drinks = Drinks(dic: dic)
+                return drink
+            })
+            completion(drinks ?? [Drinks]())
+        }
+    }
+}
