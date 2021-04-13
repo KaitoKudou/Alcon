813c5f2d327f9242e57709a379240688c2228551
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-14 03:15:15 +0900

    [Add]セル押下時に前の画面に遷移

--- alcon/alcon/ViewControllers/DrinkRecordViewController.swift
+++ alcon/alcon/ViewControllers/DrinkRecordViewController.swift
@@ -54,4 +54,8 @@ extension DrinkRecordViewController: UITableViewDelegate, UITableViewDataSource 
         return 50
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
