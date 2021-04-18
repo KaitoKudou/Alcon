087c6bf93d77e08686320c88ac3da93f701eb448
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-04-18 15:17:51 +0900

    [Add]日付別飲酒記録をfirestoreから読み出し

--- alcon/alcon/ViewModels/DrinkTableViewModel.swift
+++ alcon/alcon/ViewModels/DrinkTableViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by 工藤海斗 on 2021/04/18.
 //
 
+import FirebaseFirestore
 import Foundation
+
+class DrinkTableViewModel {
+    
+    func fetchDailyDrinkList() {
+        let firestore = Firestore.firestore()
+        firestore.collection("users").document(UIDevice.current.identifierForVendor!.uuidString).collection("drink").getDocuments { [weak self] (querySnapshots, err) in
+            guard self != nil else { return }
+            
+            if let err = err {
+                print("日付別飲酒記録読み込み失敗: \(err)")
+                return
+            }
+            
+            let drinks = querySnapshots?.documents.map({ (snapshot) -> Drinks in
+                let dic = snapshot.data()
+                let drink: Drinks = Drinks(dic: dic)
+                return drink
+            })
+            print(drinks ?? [Drinks]())
+        }
+    }
+}
