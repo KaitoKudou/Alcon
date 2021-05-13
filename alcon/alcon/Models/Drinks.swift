bb1b750d5f910b375f5e68995fc8c6768f28b39d
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-05-13 21:19:04 +0900

    [Add]日付を追加

--- alcon/alcon/Models/Drinks.swift
+++ alcon/alcon/Models/Drinks.swift
@@ -11,10 +11,12 @@ struct Drinks {
     let type: String?
     let capacity: Int?
     let pureAlcohol: Int?
+    let date: String?
     
     init(dic: [String: Any]) {
         self.type = dic["type"] as? String ?? ""
         self.capacity = dic["capacity"] as? Int ?? 0
         self.pureAlcohol = dic["pureAlcohol"] as? Int ?? 0
+        self.date = dic["date"] as? String ?? ""
     }
 }
